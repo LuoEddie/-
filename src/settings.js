697d34321769d8db4124854fd09bcb0b844f339f
LuoEddie <102347808+LuoEddie@users.noreply.github.com>
2022-03-27 17:20:36 +0800

    人资项目初始化

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,18 @@
 module.exports = {
-
-  title: 'Vue Admin Template',
+  // 修改项目的标题
+  title: '黑马人资管理系统',
 
   /**
+   * 控制头部是否固定
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: false,
+  fixedHeader: true,
 
   /**
+   * 是否显示侧边栏的logo
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false
+  sidebarLogo: true
 }
